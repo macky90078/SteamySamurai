a8ae58bc99384a7cfb224dee2155c2275f105ecf
Kevin Tieu <nivektieu@gmail.com>
2018-02-16 16:52:39 -0500

    Added basic enemy movement

---
+++ Assets/Scripts/TestScripts/playerCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCube : MonoBehaviour {
+    public bool hasCube;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Cube switched to other player.");
+
+            if (hasCube == true)
+                hasCube = false;
+            else if (hasCube == false)
+                hasCube = true;
+        }
+    }
+}
