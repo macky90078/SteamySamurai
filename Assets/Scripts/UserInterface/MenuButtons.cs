eea5a04a93a157bd7f1b3560a95decab40dc02c0
David <david_lee113@outlook.com>
2018-03-09 09:57:21 -0500

    Created basic game loop with simple menus

---
+++ Assets/Scripts/UserInterface/MenuButtons.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+    [Tooltip("The name of the scene you want to load")]
+        [SerializeField] private string sceneName;
+    [Tooltip("The canvas to be removed when game resumed")]
+        [SerializeField] private Transform pauseCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void loadScene()
+    {
+        GameManager.NextScene = sceneName;
+        SceneManager.LoadScene("LoadingScreen");
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseCanvas.gameObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit ();
+        #endif
+    }
+
+}
---
+++ Assets/Scripts/UserInterface/MenuSelect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Rewired;
+
+public class MenuSelect : MonoBehaviour {
+
+    public EventSystem eventSystem;
+    public GameObject selectedButton;
+    private bool buttonSelected;
+    Player player;
+
+	// Use this for initialization
+	void Start () {
+        player = ReInput.players.GetPlayer(0);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(player.GetAxisRaw("UIVertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedButton);
+            buttonSelected = true;
+        }
+	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
